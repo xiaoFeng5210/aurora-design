433231335e23adc5514fb6512c037eb590257f25
Eadweard <aurora.coder.zqf@gmail.com>
2024-10-11 16:02:42 +0800

    eslint fix

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,6 +1,5 @@
 // index.ts
-import { App, defineComponent } from 'vue'
 import Counter from './test/Counter.vue'
 import AuVirtualScrolling from './virtual-scrolling/AuVirtualScrolling.vue'
 
-export { Counter, AuVirtualScrolling }
+export { AuVirtualScrolling, Counter }
