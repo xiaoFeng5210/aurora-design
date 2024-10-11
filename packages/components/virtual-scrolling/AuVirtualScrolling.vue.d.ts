608bfc445867742d0ae4a169a10fe6ae35062b45
Eadweard <aurora.coder.zqf@gmail.com>
2024-10-11 16:01:12 +0800

    fix eslint

--- packages/components/virtual-scrolling/AuVirtualScrolling.vue.d.ts
+++ packages/components/virtual-scrolling/AuVirtualScrolling.vue.d.ts
@@ -1,4 +1,4 @@
-import { DefineComponent } from 'vue'
+import type { DefineComponent } from 'vue'
 
 declare const AuVirtualScrolling: DefineComponent<{}, {}, any>
 export default AuVirtualScrolling
