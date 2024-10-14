0d737a5efc7a060c36acada4c41ed82db76d5820
Eadweard <aurora.coder.zqf@gmail.com>
2024-10-14 17:41:54 +0800

    fix all

--- packages/components/line-loading/InlineLoading.vue.d.ts
+++ packages/components/line-loading/InlineLoading.vue.d.ts
@@ -1,4 +1,13 @@
 import type { DefineComponent } from 'vue'
 
-declare const InlineLoading: DefineComponent<{}, {}, any>
+declare const InlineLoading: DefineComponent<{
+  fillColor: {
+    type: StringConstructor
+    default: string
+  }
+  type: {
+    type: StringConstructor
+    default: string
+  }
+}, {}, any>
 export default InlineLoading
